97163c1cf08de22c146d73b1fe6d73d7bf2281bb
Usales <gabrielhenriquessales1@gmail.com>
2023-11-29 20:06:34 -0300

    Tela fazer denuncia 1#

--- src/app/modules/admin/menu/menu.component.ts
+++ src/app/modules/admin/menu/menu.component.ts
@@ -1,16 +1,33 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { UntypedFormBuilder, UntypedFormGroup, NgForm, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Component } from '@angular/core';
+
 
 @Component({
     selector     : 'menu',
     templateUrl  : './menu.component.html',
-    encapsulation: ViewEncapsulation.None
 })
-export class menuComponent
-{
+export class menuComponent {
     /**
      * Constructor
      */
-    constructor()
-    {
-    }
+    constructor() {}
+}
+
+export class AppComponent {
+  myForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      id: ['', Validators.required],
+      status: ['', Validators.required],
+      descricao: ['', Validators.required],
+      parecerTecnico: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    // Aqui você pode acessar os dados do formulário usando this.myForm.value
+    console.log(this.myForm.value);
+  }
 }
