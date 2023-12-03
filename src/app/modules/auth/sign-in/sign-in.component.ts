ea02d8b4d5bb8bb409d4b7c9db0c74323e24b8b9
RubensCarvalhoRocha <rocharubens4@gmail.com>
2023-12-03 15:14:40 -0300

    personalizando login

--- src/app/modules/auth/sign-in/sign-in.component.ts
+++ src/app/modules/auth/sign-in/sign-in.component.ts
@@ -45,7 +45,7 @@ export class AuthSignInComponent implements OnInit
     {
         // Create the form
         this.signInForm = this._formBuilder.group({
-            email     : ['hughes.brian@company.com', [Validators.required, Validators.email]],
+            cpf     : ['hughes.brian@company.com', [Validators.required, Validators.email]],
             password  : ['admin', Validators.required],
             rememberMe: ['']
         });
@@ -98,7 +98,7 @@ export class AuthSignInComponent implements OnInit
                     // Set the alert
                     this.alert = {
                         type   : 'error',
-                        message: 'Wrong email or password'
+                        message: 'CPF ou Senha incorretos'
                     };
 
                     // Show the alert
